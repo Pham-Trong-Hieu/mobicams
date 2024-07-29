a8949bc42fa1c49cdf634b037e528545b353510f
phamhieu <20062501.hieu@student.iuh.edu.vn>
2024-07-29 20:56:36 +0700

    push

---
+++ src/configs/db.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const db_connetion = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+
+}
+module.exports = db_connetion;
